e4ece9d26625edf9bcdadf6201e748df1058bebd
Vadim St <vadim_st_13@mail.ru>
2017-09-12 15:46:18 +0300

    complete class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	
+    }
 
     setRange(min, max) {
-
+    	this.min = min;
+    	this.max = max;
     }
 
     guess() {
-
+    	let mid = Math.round((this.max - this.min)/2 + this.min);
+    	return mid;
     }
 
     lower() {
-
+    	this.max = this.guess();
     }
 
     greater() {
-
+    	this.min = this.guess();
     }
 }
 
